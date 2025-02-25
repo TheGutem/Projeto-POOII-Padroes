e352326870c44dbdefb12a2a55c2612339655faa
Gutemberg Silva Santos <gutemsocial@gmail.com>
2025-02-25 09:42:00 -0300

    Classe que precisa receber JSON
    A classe ExportData precisa ter seu XML gerado convertido para JSON para ser utilizado no sistema do MEC

---
+++ classes/ImportDataMEC.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.padroes;
+
+/**
+ *
+ * @author Gutem
+ */
+
+//VOCÊ NÃO DEVE MODIFICAR ESTA CLASSE
+public class ImportDataMEC {
+
+    public void importData(String json){
+        //NÃO IREMOS UTILIZAR A LIB ORG.JSON PARA NÃO DEIXAR A TAREFA MAIS COMPLEXA
+        //PEGUE A SAIDA A SEGUIR E VALIDE EM: https://jsonformatter.curiousconcept.com
+        System.out.println(json);
+
+    }
+    
+}
